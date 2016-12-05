f96e9114e96b741d2c4c3458f027d773e5038dcc
ariel wilson <ariel.wilson@mymona.uwi.edu>
2016-12-05 14:37:11 +0000

    database query in login php not retreieving result hence the command to close database after the query was tried

--- login.php
+++ login.php
@@ -32,10 +32,11 @@
 			$results= mysql_query($sql);
 	
 			//mysql_query("INSERT INTO Users(first_name, last_name, username, email, last_login, password_digest, salt) VALUES ('$fname', '$lname', '$uname', '$email', '$track', '$digest', '$thesalt')");			
-			//mysql_close($database);			
+			mysql_close($database);			
 			//include_once 'confirm.php';
 		
-		
+		echo($results);
+		echo('testing');
 		
 		if ($results){
 			if(mysql_num_rows($results) > 0){
